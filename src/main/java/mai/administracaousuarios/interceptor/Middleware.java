08ea3139e18747e886bf73cb13414970df0bfccf
Roberto <roberto.menesantos@gmail.com>
2024-05-06 10:29:07 -0300

    feat: creating the middleware filter for authenticating the access to the api

---
+++ src/main/java/mai/administracaousuarios/interceptor/Middleware.java
@@ -0,0 +1,46 @@
+package mai.administracaousuarios.interceptor;
+
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class Middleware implements Filter {
+
+
+
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        String login;
+        String password;
+
+        try{
+            login = request.getHeaders("Api_Login").nextElement();
+            password = request.getHeaders("Api_Password").nextElement();
+
+
+
+        }catch (NullPointerException e){
+            System.out.println(e.getMessage());
+            ((HttpServletResponse) servletResponse).setStatus(422);
+            servletResponse.getOutputStream().write("Missing the api login or password".getBytes());
+            return;
+        }
+
+        if(login.equals(System.getenv("API_FUNC_LOGIN")) && password.equals(System.getenv("API_FUNC_PASSWORD"))){
+            filterChain.doFilter(request, servletResponse);
+        }else{
+            ((HttpServletResponse) servletResponse).setStatus(401);
+            servletResponse.getOutputStream().write("No authorized".getBytes());
+        }
+    }
+
+}

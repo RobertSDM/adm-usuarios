9c2317ba11e63ac66e30d99c7fab9bca21e69a1d
Roberto <roberto.menesantos@gmail.com>
2024-05-07 10:27:59 -0300

    refactor: changing the SLF4J import

--- src/main/java/mai/administracaousuarios/rest/LogradouroRest.java
+++ src/main/java/mai/administracaousuarios/rest/LogradouroRest.java
@@ -1,9 +1,9 @@
 package mai.administracaousuarios.rest;
 
 import jakarta.validation.Valid;
-import lombok.extern.slf4j.Slf4j;
 import mai.administracaousuarios.model.Logradouro;
 import mai.administracaousuarios.repositories.LogradouroRepository;
+import org.apache.logging.slf4j.SLF4JLogger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +19,7 @@ public class LogradouroRest {
 
     @Autowired
     private LogradouroRepository logradouroRep;
-    private final Logger logger = LoggerFactory.getLogger(Slf4j.class);
+    private final Logger logger = LoggerFactory.getLogger(SLF4JLogger.class);
 
     @GetMapping(value = "/find/all")
     public ResponseEntity<List<Logradouro>> findAll() {
